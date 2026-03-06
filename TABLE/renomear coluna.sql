4108db0b74d00042281f2d59391ad0c4d37e281c
AlfredoBk <garciabaca0711@gmail.com>
2026-03-06 17:19:25 -0300

    Add files via upload

---
+++ TABLE/renomear coluna.sql
@@ -0,0 +1,2 @@
+-- Active: 1772562532889@@127.0.0.1@3306
+ALTER TABLE teste RENAME COLUMN saude to health;
---
+++ desafio/cnpj.sql
@@ -0,0 +1,2 @@
+-- Active: 1772562532889@@127.0.0.1@3306
+ frutaria SET cnpj = 0 WHERE cnpj IS NULL;
